def19820f3803ae7b1120f3e76d94fcda99cce1c
mmalumbo <mmalumbo@gmail.com>
2019-06-19 11:06:49 +0200

    added logs.
    Signed-off-by: mmalumbo <mmalumbo@gmail.com>

--- src/log-worker/logWorker.ts
+++ src/log-worker/logWorker.ts
@@ -6,15 +6,22 @@ import { PusherLogger } from '../Logger';
 const { log, error } = console;
 
 export const logWorker = async (config: DotenvParseOutput): Promise<void> => {
+  console.log('ready to recieve a message');
+  console.log();
+
   try {
     const worker: Worker = await createWorker(config);
 
     const processLog = async (message: string, acknowledgment: () => void) => {
       const parsedMessage: Message = JSON.parse(message);
-      log('\n', parsedMessage, '\n');
+
+      log('received message: ')
+      log(parsedMessage);
+
 
       const pusherLogger = new PusherLogger(config, parsedMessage);
       pusherLogger.info(parsedMessage.message);
+
 
       acknowledgment();
     };
